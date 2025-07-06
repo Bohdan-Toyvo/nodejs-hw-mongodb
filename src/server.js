1319c4469a66bc9cdf8b8c9e50a5556426939e6d
Bohdan-Toyvo <toyvo7@gmail.com>
2025-07-06 20:27:42 +0200

    Revert "feat: get all tasks"

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
 import dotenv from 'dotenv';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import { PORT } from './utils/env.js';
 
 dotenv.config();
 
@@ -21,55 +19,6 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' });
-  });
-
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      logger.error('Error fetching contacts:', error.message);
-      res.status(500).json({
-        status: 500,
-        message: 'Failed to fetch contacts',
-        error: error.message,
-      });
-    }
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
-
-      if (!contact) {
-        return res.status(404).json({
-          message: 'Contact not found',
-        });
-      }
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      logger.error(
-        `Error fetching contact by ID (${req.params.contactId}):`,
-        error.message,
-      );
-
-      res.status(500).json({
-        status: 500,
-        message: 'Failed to fetch contact',
-        error: error.message,
-      });
-    }
   });
 
   app.use((req, res) => {
