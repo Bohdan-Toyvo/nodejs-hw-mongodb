6fdad2cf670b94675082cad53bcd45dc114c398a
Bohdan <toyvo7@gmail.com>
2025-07-18 18:37:19 +0200

    add validation for requests and IDs

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,36 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import validateBody from '../middlewares/validateBody.js';
+import {
+  addContactSchema,
+  updateContactSchema,
+} from '../validation/contactsValidation.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const contactsRouter = Router();
 
 contactsRouter.get('/', ctrlWrapper(getContactsController));
-contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
-contactsRouter.post('/', ctrlWrapper(createContactController));
-contactsRouter.patch('/:contactId', ctrlWrapper(patchContactController));
-contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
+contactsRouter.get(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+contactsRouter.post(
+  '/',
+  validateBody(addContactSchema),
+  ctrlWrapper(createContactController),
+);
+contactsRouter.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
+contactsRouter.delete(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default contactsRouter;
