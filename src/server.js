ce2821ae90d206981c928a02afa584a04b52536e
Bohdan <toyvo7@gmail.com>
2025-07-30 22:25:56 +0200

    modify

--- src/server.js
+++ src/server.js
@@ -16,8 +16,8 @@ const logger = pino();
 export const setupServer = () => {
   const app = express();
 
-  app.use(cors());
   app.use(express.json());
+  app.use(cors());
   app.use(cookieParser());
 
   app.use((req, res, next) => {
